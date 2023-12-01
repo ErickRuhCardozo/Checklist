9ffcbed3add656d5ce4d0c26d2ce7b0800cffce0
Erick Ruh Cardozo <erickruhcardozo98@gmail.com>
2023-12-01 15:10:34 -0300

    Shipping new Admin's Unities Screen

--- resources/views/admin/unities/index.blade.php
+++ resources/views/admin/unities/index.blade.php
@@ -1,26 +1,35 @@
+@push('styles')
+<style>
+    @media (max-width: 575.98px) {
+        .hoverable-card { width: 11rem !important; }
+    }
+</style>
+@endpush
 <x-app title="Unidades Cadastradas">
     <x-slot name="rightBodySection"> @include ('components.partials.admin.menu', ['selected' => 'unities.index']) </x-slot>
 
     @if ($unities->isEmpty())
         <p class="lead text-center">Nenhuma Unidade Cadastrada</p>
     @else
-        <div class="table-responsive mb-2">
-            <table class="table table-hover">
-                <thead>
-                    <th>@sortablelink('name', 'Nome')</th>
-                    <th>@sortablelink('places', 'Ambientes')</th>
-                    <th>@sortablelink('users', 'Usuários')</th>
-                </thead>
-                <tbody class="table-group-divider">
-                    @foreach ($unities as $unity)
-                        <tr onclick="location.assign('{{ route('admin.unities.show', $unity->id) }}')">
-                            <td>{{ $unity->name }}</td>
-                            <td>{{ $unity->places()->count() }}</td>
-                            <td>{{ $unity->users()->count() }}</td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="d-flex justify-content-center gap-2 flex-wrap">
+            @foreach ($unities as $unity)
+                <div class="card hoverable-card" style="width: 14rem;" onclick="location.assign('{{ route('admin.unities.show', $unity->id) }}')">
+                    <div class="d-flex flex-column" style="width: fit-content; max-width: 100%;">
+                        <span class="badge text-bg-primary text-start d-flex align-items-center" style="border-bottom-left-radius: 0; border-bottom-right-radius: 0;">
+                            <i class="bi bi-person-fill me-1"></i>
+                            <span class="text-truncate" style="font-size: 0.6rem;">{{ $unity->users()->count() }} Usuários</span>
+                        </span>
+                        <span class="badge text-bg-info text-start d-flex align-items-center" style="border-top-left-radius: 0; border-bottom-left-radius: 0; border-top-right-radius: 0;">
+                            <i class="bi bi-geo-alt-fill me-1"></i>
+                            <span style="font-size: 0.6rem;">{{ $unity->places()->count() }} Ambientes</span>
+                        </span>
+                    </div>
+                    <div class="card-body d-flex align-items-center justify-content-center"><i class="bi bi-buildings-fill" style="font-size: 4rem;"></i></div>
+                    <div class="card-footer d-flex justify-content-center align-items-center" style="min-height: 73px;">
+                        <h5 class="hoverable-card-title text-center">{{ $unity->name }}</h5>
+                    </div>
+                </div>
+            @endforeach
         </div>
     @endif
 
